f541777c1ba05bf8e169b14b9be68177834c744e
Philipp Hanemann <philipp.hanemann@posteo.de>
2021-06-02 08:36:15 +0200

    refator: using left shift operator instead of math package

--- grains/grains.go
+++ grains/grains.go
@@ -4,19 +4,16 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 func Square(x int) (square uint64, err error) {
 	if x < 1 || x > 64 {
 		return 0, errors.New("input is out of range")
 	}
-	return uint64(math.Pow(2, float64(x-1))), nil
+	return uint64(1 << (x - 1)), nil
 }
 
 func Total() uint64 {
-	var res uint64
-	res = uint64(math.Pow(2, 63))
-	return 2*res - 1
+	return uint64(1<<64 - 1)
 
 }
