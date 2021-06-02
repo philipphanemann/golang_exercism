9ba22d2f5d617853969ee4b626e2170ffe3f6b26
Philipp Hanemann <philipp.hanemann@posteo.de>
2021-06-02 14:26:14 +0200

    New function working

--- clock/clock.go
+++ clock/clock.go
@@ -1,1 +1,29 @@
 package clock
+
+import "fmt"
+
+const base int = 60
+const MinutesPerDay int = 60 * 24
+
+type OwnTime struct {
+	h, m int
+}
+
+// MinutesToTime returns positive hour and minutes
+func MinutesToTime(m int) (hour int, min int) {
+	m %= MinutesPerDay
+	if m < 0 {
+		m = MinutesPerDay + m
+	}
+	return m / base, m % base
+}
+
+func New(h, m int) OwnTime {
+	totalMinutes := h*base + m
+	h, m = MinutesToTime(totalMinutes)
+	return OwnTime{h, m}
+}
+
+func (t OwnTime) String() string {
+	return fmt.Sprintf("%02d:%02d", t.h, t.m)
+}
