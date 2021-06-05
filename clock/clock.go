1185212109d5be65b9a7c3936f8d3a828109360d
Philipp Hanemann <philipp.hanemann@posteo.de>
2021-06-05 14:43:24 +0200

    refactor: formatting

--- clock/clock.go
+++ clock/clock.go
@@ -3,38 +3,45 @@ package clock
 import "fmt"
 
 const base int = 60
-const MinutesPerDay int = 60 * 24
+const minutesPerDay int = 60 * 24
 
+// OwnTime time with hours and minutes
 type OwnTime struct {
 	h, m int
 }
 
 // MinutesToTime returns positive hour and minutes
 func MinutesToTime(m int) OwnTime {
-	m %= MinutesPerDay
+	m %= minutesPerDay
 	if m < 0 {
-		m = MinutesPerDay + m
+		m = minutesPerDay + m
 	}
 	return OwnTime{m / base, m % base}
 }
 
+// TimeToMinutes returns total minutes of time
 func TimeToMinutes(t OwnTime) int {
 	return t.h*base + t.m
 }
+
+// New returns positive time of time.
 func New(h, m int) OwnTime {
 	totalMinutes := TimeToMinutes(OwnTime{h, m})
 	return MinutesToTime(totalMinutes)
 }
 
+// String formats time output
 func (t OwnTime) String() string {
 	return fmt.Sprintf("%02d:%02d", t.h, t.m)
 }
 
+// Add adds minutes to time
 func (t OwnTime) Add(minutes int) OwnTime {
 	m := TimeToMinutes(t) + minutes
 	return MinutesToTime(m)
 }
 
+// Subtract subtracts minutes from time
 func (t OwnTime) Subtract(minutes int) OwnTime {
 	m := TimeToMinutes(t) - minutes
 	return MinutesToTime(m)
