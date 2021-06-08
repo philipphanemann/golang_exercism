27a8dc12d9117cf22db712c9e75a4a7591638ba9
Philipp Hanemann <philipp.hanemann@posteo.de>
2021-06-08 18:16:59 +0200

    refactor: renaming

--- clock/clock.go
+++ clock/clock.go
@@ -2,37 +2,34 @@ package clock
 
 import "fmt"
 
-const base int = 60
-const minutesPerDay int = 60 * 24
-
-// OwnTime time in minutes
-type OwnTime struct {
+// Clock time in minutes
+type Clock struct {
 	m int
 }
 
 // New returns positive time of time.
-func New(h, m int) OwnTime {
-	m = h*base + m
-	m %= minutesPerDay
+func New(h, m int) Clock {
+	m = h*60 + m
+	m %= 60 * 24
 	if m < 0 {
-		return OwnTime{m + minutesPerDay}
+		return Clock{m + 60*24}
 	}
-	return OwnTime{m}
+	return Clock{m}
 }
 
 // String formats time output
-func (t OwnTime) String() string {
-	hours := t.m / base
-	minutes := t.m % base
+func (t Clock) String() string {
+	hours := t.m / 60
+	minutes := t.m % 60
 	return fmt.Sprintf("%02d:%02d", hours, minutes)
 }
 
 // Add adds minutes to time
-func (t OwnTime) Add(minutes int) OwnTime {
+func (t Clock) Add(minutes int) Clock {
 	return New(0, t.m+minutes)
 }
 
 // Subtract subtracts minutes from time
-func (t OwnTime) Subtract(minutes int) OwnTime {
+func (t Clock) Subtract(minutes int) Clock {
 	return New(0, t.m-minutes)
 }
