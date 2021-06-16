a873b3837d10fd75005fed3ea77838d65c8ac0d8
Philipp Hanemann <philipp.hanemann@posteo.de>
2021-06-16 09:47:27 +0200

    using channel, no benchmark improvements

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -3,6 +3,8 @@ package letter
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
+var counter FreqMap
+
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
 func Frequency(s string) FreqMap {
@@ -13,13 +15,31 @@ func Frequency(s string) FreqMap {
 	return m
 }
 
-func ConcurrentFrequency(txts []string) FreqMap {
+func FrequencyGoroutine(s string, c chan FreqMap) {
 	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	c <- m
+}
+
+func ConcurrentFrequency(txts []string) FreqMap {
+	counts := FreqMap{}
+	c := make(chan FreqMap, 6)
+
 	for _, txt := range txts {
-		for i, count := range Frequency(txt) {
-			m[i] += count
-		}
+		go FrequencyGoroutine(txt, c)
+	}
+	x, y, z := <-c, <-c, <-c
+	for i, count := range x {
+		counts[i] += count
+	}
+	for i, count := range y {
+		counts[i] += count
 	}
 
-	return m
+	for i, count := range z {
+		counts[i] += count
+	}
+	return counts
 }
