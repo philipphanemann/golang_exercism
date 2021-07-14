87145cbb2fbf3b8bfc1f2d464c7df1c374867f2f
Philipp Hanemann <philipp.hanemann@posteo.de>
2021-07-14 09:50:13 +0200

    added some exhaustion catch

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,9 +1,13 @@
 package robotname
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
+
+var created = map[string]int{}
+var ubNames = 26 * 26 * 10 * 10 * 10
 
 // Robot type
 type Robot struct {
@@ -13,8 +17,22 @@ type Robot struct {
 // Name creates a robot name
 func (r *Robot) Name() (string, error) {
 	rand.Seed(time.Now().UnixNano())
+	var name string
 	if r.name == "" {
-		r.name = string([]rune{R('A', 25), R('A', 25), R('0', 9), R('0', 9), R('0', 9)})
+		if len(created) >= ubNames {
+			return "", fmt.Errorf("names are exhausted")
+		}
+		for {
+			name = createName()
+			if _, ok := created[name]; ok {
+				name = createName()
+			} else {
+				created[name] = 1
+				break
+			}
+
+		}
+		r.name = name
 	}
 	return r.name, nil
 }
@@ -27,4 +45,9 @@ func (r *Robot) Reset() {
 // R returns a random rune in range r starting at offset o.
 func R(o, r int) rune {
 	return rune(rand.Intn(r) + o)
+}
+
+//createName randomly creates a name
+func createName() string {
+	return string([]rune{R('A', 25), R('A', 25), R('0', 9), R('0', 9), R('0', 9)})
 }
